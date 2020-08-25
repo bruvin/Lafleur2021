3d7a1acd19c57367883bd4c9ba1d93588ed683ca
Bruno Vincent <brn.vincent@neuf.fr>
2020-08-25 11:27:20 +0200

    Commit initial du projet

---
+++ appli/include/_bdGestionDonnees.lib.php
@@ -0,0 +1,100 @@
+<?php
+
+// FONCTIONs POUR L'ACCES A LA BASE DE DONNEES
+// Ajouter en têtes 
+// Voir : jeu de caractères à la connection
+
+/** 
+ * Se connecte au serveur de données                     
+ * Se connecte au serveur de données à partir de valeurs
+ * prédéfinies de connexion (hôte, compte utilisateur et mot de passe). 
+ * Retourne l'identifiant de connexion si succès obtenu, le booléen false 
+ * si problème de connexion.
+ * @return resource identifiant de connexion
+ */
+function connecterServeurBD() 
+{
+    $PARAM_hote='localhost'; // le chemin vers le serveur
+    $PARAM_port='3306';
+    $PARAM_nom_bd='baseLafleur1'; // le nom de votre base de données
+    $PARAM_utilisateur='root'; // nom d'utilisateur pour se connecter
+    $PARAM_mot_passe=''; // mot de passe de l'utilisateur pour se connecter
+
+    $connect = new PDO('mysql:host='.$PARAM_hote.';port='.$PARAM_port.';dbname='.$PARAM_nom_bd, $PARAM_utilisateur, $PARAM_mot_passe);
+ 
+    return $connect;
+}
+
+function lister()
+{
+    $connexion = connecterServeurBD();
+   
+    $requete="select * from produit";
+    
+    $jeuResultat=$connexion->query($requete); // on va chercher tous les membres de la table qu'on trie par ordre croissant
+  
+    $i = 0;
+    $ligne = $jeuResultat->fetch();
+    while($ligne)
+    {
+        $fleur[$i]['image']=$ligne['pdt_image'];
+        $fleur[$i]['ref']=$ligne['pdt_ref'];
+        $fleur[$i]['designation']=$ligne['pdt_designation'];
+        $fleur[$i]['prix']=$ligne['pdt_prix'];
+        $ligne=$jeuResultat->fetch();
+        $i = $i + 1;
+    }
+    $jeuResultat->closeCursor();   // fermer le jeu de résultat
+  
+  return $fleur;
+}
+
+
+function ajouter($ref, $des, $prix, $image, $cat,&$tabErr)
+{
+  // Ouvrir une connexion au serveur mysql en s'identifiant
+  $connexion = connecterServeurBD();
+    
+    // Vérifier que la référence saisie n'existe pas déja
+    $requete="select * from produit";
+    $requete=$requete." where pdt_ref = '".$ref."';"; 
+    $jeuResultat=$connexion->query($requete); // on va chercher tous les membres de la table qu'on trie par ordre croissant
+  
+    //$jeuResultat->setFetchMode(PDO::FETCH_OBJ); // on dit qu'on veut que le résultat soit récupérable sous forme d'objet     
+    
+    $ligne = $jeuResultat->fetch();
+    if($ligne)
+    {
+      $message="Echec de l'ajout : la référence existe déjà !";
+      ajouterErreur($tabErr, $message);
+    }
+    else
+    {
+      // Créer la requête d'ajout 
+       $requete="insert into produit"
+       ."(pdt_ref,pdt_designation,pdt_prix,pdt_image, pdt_categorie) values ('"
+       .$ref."','"
+       .$des."',"
+       .$prix.",'"
+       .$image."','"
+       .$cat."');";
+     
+        // Lancer la requête d'ajout 
+        $ok=$connexion->query($requete); // on va chercher tous les membres de la table qu'on trie par ordre croissant
+          
+        // Si la requête a réussi
+        if ($ok)
+        {
+          $message = "La fleur a été correctement ajoutée";
+          ajouterErreur($tabErr, $message);
+        }
+        else
+        {
+          $message = "Attention, l'ajout de la fleur a échoué !!!";
+          ajouterErreur($tabErr, $message);
+        } 
+  
+    }
+}
+
+?>
---
+++ appli/vues/menu.php
@@ -0,0 +1,33 @@
+
+  <!-- Menu
+    ================================================== -->
+    <div class="navbar navbar-inverse navbar-fixed-top">
+      <div class="navbar-inner">
+        <div class="container">
+          <button type="button" class="btn btn-navbar" data-toggle="collapse" data-target=".nav-collapse">
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+          <a class="brand" href="./indexzz.php">Accueil</a>
+          <div class="nav-collapse collapse">
+            <ul class="nav">
+              <li class="active">
+                <a href="./indexzz.php">Accueil</a>
+              </li>
+              <li class="nav">
+                <a href="lister.php">Lister</a>
+              </li>
+              <li class="nav">
+                <a href="rechercher.php">Rechercher</a>
+              </li>
+              <li class="nav"> 
+                <a href="ajouter.php" >Ajouter</a>  
+              </li>              
+            </ul>
+          </div>
+        </div>
+      </div>
+    </div>
+</div>
+
